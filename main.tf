217aed9227d018eeb8f6aa1574f59c513b38525f
wahed <hm682264@dal.ca>
2024-07-06 18:52:32 -0300

    feat: use ASG instead of a single EC2 instance

--- main.tf
+++ main.tf
@@ -2,22 +2,32 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-0c55b159cbfafe1f0"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0fb653ca2d3203ac1"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
+              nohup python -m SimpleHTTPServer ${var.server_port} &
               EOF
 
-  user_data_replace_on_change = true
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnet.default.ids
+  min_size             = 2
+  max_size             = 4
 
-  tags = {
-    Name = "terraform-example"
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
 
--- variables.tf
+++ variables.tf
@@ -8,3 +8,14 @@ output "public_ip" {
   value       = aws_instance.example.public_ip
   description = "value of the public IP of the EC2 instance"
 }
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
